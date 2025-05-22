d483c9998908998766324964946579cd5726cf58
akfahad47 <hello.akfahad@gmail.com>
2025-05-22 16:13:28 -0400

    Varify JWT Token

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -2,6 +2,7 @@
 using WebAPIDemo.Data;
 using WebAPIDemo.Filters;
 using WebAPIDemo.Filters.ActionFilters;
+using WebAPIDemo.Filters.AuthFilters;
 using WebAPIDemo.Filters.ExceptionFilters;
 using WebAPIDemo.Models;
 using WebAPIDemo.Models.Repositories;
@@ -12,6 +13,7 @@ namespace WebAPIDemo.Controllers
 
 	[ApiController]
 	[Route("api/[controller]")]
+	[JwtTokenAuthFilter]
 	public class ShirtsController : ControllerBase
 	{
         public ApplicationDbContext Db { get; }
