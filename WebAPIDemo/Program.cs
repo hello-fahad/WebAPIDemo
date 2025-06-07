10634bc9afc20848bf8c52716e61670085374879
akfahad47 <hello.akfahad@gmail.com>
2025-06-07 12:08:23 -0400

    Swagger document support and Added authorization header to Swagger

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -1,19 +1,50 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using WebAPIDemo.Data;
+using WebAPIDemo.Filters.OperationFilter;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ShirtStoreManagement"));
 });
+
+// Add services to the container
 builder.Services.AddControllers();
-builder.Services.AddCors(); // Add CORS
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.OperationFilter<AuthorizationHeaderOperationFilter>();
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+
+});
+
 var app = builder.Build();
-app.UseCors(builder => builder
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader());
+
+// Configure the HTTP request pipeline.
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
 
